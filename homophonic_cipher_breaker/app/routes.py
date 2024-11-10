b9ccdcbc7d7c1442d97b58d79504cb0205349208
Sumeet <bt22csd054@iiitn.ac.in>
2024-11-10 17:53:54 +0530

    updates
    final

--- homophonic_cipher_breaker/app/routes.py
+++ homophonic_cipher_breaker/app/routes.py
@@ -1,6 +1,6 @@
 # app/routes.py
 from flask import Blueprint, request, render_template, jsonify
-from .cipher_breaker import load_dataset, initial_key_setup, hill_climb
+from .cipher_breaker import load_dataset, initial_key_setup, hill_climb,apply_key
 
 main = Blueprint('main', __name__)
 
@@ -15,14 +15,13 @@ def process_cipher():
 
     key_map = initial_key_setup(ciphertext)
     best_key, best_score = hill_climb(ciphertext, key_map)
+    plaintext = apply_key(ciphertext, best_key)
+
+    formatted_key_mapping = {letter: ", ".join(symbols) for letter, symbols in best_key.items()}
 
     return jsonify({
-        'key_mapping': best_key,
-        'score': best_score,
-        'plaintext': apply_key(ciphertext, best_key)
+        'key_mapping': formatted_key_mapping,
+        'cipher_text': ciphertext,
+        'decoded_message': plaintext,  # Output decoded message
+        'score': best_score
     })
-
-def apply_key(ciphertext, key_map):
-    # Convert ciphertext using the key_map to derive plaintext
-    plaintext = ''.join([key_map.get(char, char) for char in ciphertext])
-    return plaintext
